d6fc1ccb73a064df058afbaa913ce180e298206e
RodneiSilva <s.rodnei@hotmail.com>
2021-03-16 02:10:51 -0300

    Roteando consumo de servico SOAP e gravando em BD MySQL

---
+++ camel-alura/src/main/java/br/com/caelum/camel/RotaNegociacoesTimerBKP.java
@@ -0,0 +1,87 @@
+package br.com.caelum.camel;
+
+import java.text.SimpleDateFormat;
+
+import org.apache.camel.CamelContext;
+import org.apache.camel.Exchange;
+import org.apache.camel.Processor;
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.dataformat.xstream.XStreamDataFormat;
+import org.apache.camel.impl.DefaultCamelContext;
+import org.apache.camel.impl.SimpleRegistry;
+
+import com.mysql.cj.jdbc.MysqlConnectionPoolDataSource;
+
+import com.thoughtworks.xstream.XStream;
+
+public class RotaNegociacoesTimerBKP {
+
+	public static void main(String[] args) throws Exception {
+
+		SimpleRegistry registro = new SimpleRegistry();
+		registro.put("mysql", criaDataSource());
+		CamelContext context = new DefaultCamelContext(registro);//construtor recebe registro
+		
+		context.addRoutes(new RouteBuilder() { 
+
+		    @Override
+		    public void configure() throws Exception {
+		    	
+		    	final XStream xstream = new XStream();
+		    	//criando representacao do XML
+		    	xstream.alias("negociacao", Negociacao.class);
+		    	
+		    	//definindo componente timer com parametros de periodo
+		    	from("timer://negociacoes?fixedRate=true&delay=1s&period=10s").
+		        
+		    	//definindo a rota http que sera obtido os dados
+		    	to("http4://argentumws-spring.herokuapp.com/negociacoes").
+		    	
+		    	//convertBodyTo(String.class).
+		            
+	            //Convertendo retorno para a representação do objeto
+	            unmarshal(new XStreamDataFormat(xstream)).
+		            
+	            split(body()). //cada negociação torna uma mensagem
+		            
+		            process(new Processor() {
+		                @Override
+		                public void process(Exchange exchange) throws Exception {
+		                    Negociacao negociacao = exchange.getIn().getBody(Negociacao.class);
+		                    exchange.setProperty("preco", negociacao.getPreco());
+		                    exchange.setProperty("quantidade", negociacao.getQuantidade());
+		                    String data = new SimpleDateFormat("YYYY-MM-dd HH:mm:ss").format(negociacao.getData().getTime());
+		                    exchange.setProperty("data", data);
+		                }
+		              }).
+		            setBody(simple("insert into negociacao(preco, quantidade, data) values (${property.preco}, ${property.quantidade}, '${property.data}')")).
+		            log("${body}"). //logando o comando sql
+		            delay(1000). //esperando 1s para deixar a execução mais fácil de entender
+		        to("jdbc:mysql"); //usando o componente jdbc que envia o SQL para mysql
+		            
+		    		//setHeader(Exchange.FILE_NAME, simple("bc-${header.CamelSplitIndex}.xml")).
+		            //to("file:saidaNegociacoes");			    	
+			    	
+			    }
+	
+		});	
+			
+			context.start();
+			Thread.sleep(200000);
+			context.stop();
+}
+	
+	private static MysqlConnectionPoolDataSource criaDataSource() throws Exception {
+	    MysqlConnectionPoolDataSource mysqlDs = new MysqlConnectionPoolDataSource();
+	    mysqlDs.setDatabaseName("camel");
+	    mysqlDs.setServerName("localhost");
+	    mysqlDs.setPort(3306);
+	    mysqlDs.setUser("root");
+	    mysqlDs.setPassword("!QAZ2wsx");
+	    mysqlDs.setServerTimezone("UTC");
+	    
+	    return mysqlDs;
+	}
+	
+	
+}
--- camel-alura/src/main/java/br/com/caelum/camel/RotaPedidos.java
+++ camel-alura/src/main/java/br/com/caelum/camel/RotaPedidos.java
@@ -1,7 +1,9 @@
 package br.com.caelum.camel;
 
 import org.apache.camel.CamelContext;
+import org.apache.camel.Exchange;
 import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.component.http4.HttpMethods;
 import org.apache.camel.impl.DefaultCamelContext;
 
 public class RotaPedidos {
@@ -15,15 +17,25 @@ public class RotaPedidos {
 			    @Override
 			    public void configure() throws Exception {
 			    	from("file:pedidos").
-			    	
+			    	setProperty("pedidoId", xpath("/pedido/id/text()")).
+			    	setProperty("clienteId", xpath("/pedido/pagamento/email-titular/text()")).
+			    	log("${id}").
 			    	split().xpath("/pedido/itens/item").
-			    	filter().xpath("/item/formato[text()='IMPRESSO']").
+			    	
+			    	setProperty("ebookId", xpath("/item/livro/codigo/text()")).
+			    	
+			    	filter().xpath("/item/formato[text()='EBOOK']").
 			    		log("${id}").
 			    		marshal().xmljson().
 			    		log("${id} - ${body}").			    	  
-			    		setHeader("CamelFileName", simple("${file:name.noext}.json")).
 			    		
-			    	to("file:saida");
+			    		//setHeader("CamelFileName", simple("${file:name.noext}.json")).
+			    		//setHeader(Exchange.FILE_NAME, simple("${file:name.noext}-${header.CamelSplitIndex}.json")).
+			    		setHeader(Exchange.HTTP_METHOD, HttpMethods.GET).
+			    		setHeader(Exchange.HTTP_QUERY, simple("ebookId=${property.ebookId}&pedidoId=${property.pedidoId}&clienteId=${property.clienteId}")).
+			    		
+			    	//to("file:saida");
+			    	to("http4://localhost:8080/webservices/ebook/item");
 			    }
 	
 		});	
